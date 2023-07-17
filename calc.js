fac77ab72e0240166b7ce7154820fc2119d3c038
Sreelakshmi M S <sreelakshmi2002ms@gmail.com>
2023-07-17 15:41:19 +0530

    calculator

---
+++ calc.js
@@ -0,0 +1,12 @@
+function displayData(n){
+    result.value=result.value+n
+}
+function evaluateRes(s){
+    result.value=eval(result.value)
+}
+function AllClear(){
+    result.value=""
+}
+function del(){
+    result.value=result.value.slice(0,-1)
+}
